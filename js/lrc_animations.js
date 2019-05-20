32c1a40d80d3d6cbc8ff61ae43983b740e9d4076
Jovanie De La Cruz <jovaniedelacruz@gmail.com>
2019-05-20 15:10:01 -0500

    Added mobile nav menu.

---
+++ js/lrc_animations.js
@@ -0,0 +1,18 @@
+//Jquery Slide Animation for Mobile Nav Menu
+$(".header__banner__button").click(function() {
+	$(".header__nav__menu").stop().slideToggle(250);
+});
+//Jquery Mobile Nav Toggle
+$(document).ready(function() {
+	function checkWidth() {
+		var windowWidth = $(window).width();
+		if (windowWidth <= 930) {
+			$(".header__nav__menu").hide();
+		}
+		if (windowWidth > 930) {
+			$(".header__nav__menu").show();
+		}
+	}
+	checkWidth();
+	$(window).resize(checkWidth);
+});
