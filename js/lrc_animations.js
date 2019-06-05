d29740360176ead5e73321ff119f659ffd179fc0
Jovanie De La Cruz <jovaniedelacruz@gmail.com>
2019-06-05 09:54:51 -0500

    u

--- js/lrc_animations.js
+++ js/lrc_animations.js
@@ -1,34 +1,36 @@
 //Jquery Slide Animation for Mobile Nav Menu
-$(".header__banner__button").click(function() {
-	$(".header__nav__menu").stop().slideToggle(250);
-});
-//Jquery Mobile Nav Toggle
 $(document).ready(function() {
-	function checkWidth() {
-		var windowWidth = $(window).width();
-		if (windowWidth <= 951) {
-			$(".header__nav__menu").hide();
-		}
-		if (windowWidth > 951) {
-			$(".header__nav__menu").show();
 
-				var maxHeight = 0;
-
-				$(".content__match_height").each(function() {
-					if ($(this).height() > maxHeight) {
-						maxHeight = $(this).height();
-					}
-				});
-
-				$(".content__match_height").height(maxHeight);
-		}
-	}
-	checkWidth();
-	$(window).resize(checkWidth);
 });
+
+//Jquery Mobile Nav Toggle
+function checkWidth() {
+  var windowWidth = $(window).width();
+  if (windowWidth <= 650) {
+    $(".header__nav__menu").hide();
+  }
+  if (windowWidth > 650) {
+    $(".header__nav__menu").show();
+
+    var maxHeight = 0;
+
+    $(".content__match_height").each(function() {
+      if ($(this).height() > maxHeight) {
+        maxHeight = $(this).height();
+      }
+    });
+
+    $(".content__match_height").height(maxHeight);
+  }
+}
+checkWidth();
+$(window).resize(checkWidth);
 
 $(window).on('load', function() {
   if ($(".sidebar").height() > $(".content").height()) {
     $(".content").height($(".sidebar").height());
   }
+	$(".header__banner__button").click(function() {
+    $(".header__nav").slideToggle(500);
+  });
 });
