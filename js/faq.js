073f7bc0cbc22757ba731909b53bbdbd3ba63969
Jovanie De La Cruz <jovaniedelacruz@gmail.com>
2019-06-18 10:42:10 -0500

    FAQ JS fix & Mobile Nav Update

--- js/faq.js
+++
@@ -1,8 +0,0 @@
-$(window).on('load', function () {
-  $('.faq__menu__tag__list__entry__q').click(function () {
-    $(this).siblings('.faq__menu__tag__list__entry__content').slideToggle();
-    $(this).toggleClass('faq__menu__tag__list__entry__q--active');
-  }
-);
-  checkWidth();
-});
--- js/lrc_animations.js
+++ js/lrc_animations.js
@@ -1,13 +1,10 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-  //$(".header__banner__button").click(function() {
-  //$('.header__nav').slideToggle();
-  //});
 });
 
 // Once Document has loaded
 $(window).on('load', function () {
-  $(".header__banner__button").on('click', function() {
+  $('.header__banner__button').on('click', function () {
     $('.header__nav').slideToggle();
   });
 
@@ -16,32 +13,43 @@ $(window).on('load', function () {
 
     var windowWidth = window.innerWidth;
 
-    $(".content, .sidebar").height('auto');
+    $('.content, .sidebar').height('auto');
     if (windowWidth <= 480) {
 
       //      if (window.innerWidth < 481) {
-      //      $(".sidebar__banner").insertBefore(".content");
+      //      $('.sidebar__banner').insertBefore('.content');
       //  } else {
-      //  $(".sidebar__banner").prependTo(".sidebar");
+      //  $('.sidebar__banner').prependTo('.sidebar');
       //}
-    }
+      return 0;}
+
     if (windowWidth > 480) {
-      $(".header__nav__menu").show();
-      $(".content, .sidebar").each(function() {
+      $('.header__nav__menu').show();
+      $('.content, .sidebar').each(function () {
         if ($(this).height() > maxHeight) {
           maxHeight = $(this).height();
         }
       });
-      $(".content, .sidebar").height(maxHeight);
+
+      $('.content, .sidebar').height(maxHeight);
     }
 
     if (window.innerWidth < 767) {
-      $(".header__nav").hide();
+      $('.header__nav').hide();
     }
+
     if (window.innerWidth > 767) {
-      $(".header__nav").show();
+      $('.header__nav').show();
     }
   }
+
   checkWidth();
   $(window).resize(checkWidth);
+
+  $('.faq__menu__tag__list__entry__q').click(function () {
+    $(this).siblings('.faq__menu__tag__list__entry__content').slideToggle();
+    $(this).toggleClass('faq__menu__tag__list__entry__q--active');
+  });
+
+  checkWidth();
 });
